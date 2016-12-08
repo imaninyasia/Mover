683b6d6bbd442136ec09ba5c5728d16993000fbe
imaninyasia <imani.fecu72@ymail.com>
2016-12-08 09:17:06 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Signup_Login from './Routes/Signup_Login/Signup_Login.jsx';
+import Dashboard  from './Routes/Dashboard/Dashboard.jsx';
+import Landing from './Routes/Landing/Landing.jsx';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+ReactDOM.render(<Router history={browserHistory}>
+        <Route path='/app' component={Landing}></Route>
+        <Route path='/app/signup_login' component={Signup_Login}></Route>
+        <Route path='/app/movewatch' component={Dashboard}></Route>
+      </Router>, document.querySelector('#root-container'));
+
+
